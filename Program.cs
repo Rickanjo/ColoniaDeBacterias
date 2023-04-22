d2fba4ba62bb71c6b5085e7e5f833b5966b1c99b
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-04-22 13:34:47 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("--- Colonia de bacterias ---\n");
+
+Console.Write("Digite o número de indivíduos na colônia de bactérias: ");
+double n = double.Parse(Console.ReadLine()!);
+
+double t = 2 * Math.Log2(n / 2000);
+
+Console.WriteLine($"A colônia de bactérias atinge {n} indivíduos em {t} horas.");
